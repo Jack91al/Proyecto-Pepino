7d4807bb267cd77feaf35c28912734d03e2a4c12
DebonitaConker <107524865+DebonitaConker@users.noreply.github.com>
2023-02-04 20:14:46 -0400

    k

--- GGJ_Pepino/Assets/Scripts/Apuerta.cs
+++ GGJ_Pepino/Assets/Scripts/Apuerta.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Apuerta : MonoBehaviour
 {
+    public bool iAmSpawners;
+    public GameObject spawners;
+
     private Animator anim;
     int ab;
     bool cerrant;
@@ -12,6 +15,10 @@ public class Apuerta : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (iAmSpawners)
+        {
+            spawners.SetActive(false);
+        }
         anim = GetComponent<Animator>();
         cerrant = false;
         ab = Animator.StringToHash("abierto");
@@ -29,11 +36,14 @@ public class Apuerta : MonoBehaviour
     public void Abrir()
     {
         cerrant = true;
+        if (iAmSpawners)
+        {
+            spawners.SetActive(true);
+        }
 
     }
     public void tempo()
     {
         anim.SetBool(ab, true);
-        Debug.Log("ffff");
     }
 }
